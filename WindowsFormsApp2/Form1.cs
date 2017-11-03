a012d813145dfa4ef4086002bf84ee25a27b2550
gokhanunverdi <gokhanunverdi@CND5010WP4>
2017-11-03 13:50:29 +0300

    nonnklklhff

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string x = "asd";
+
+            x = x + "asd";
+        }
     }
 }
